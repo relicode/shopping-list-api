30963ff7e05802179388cc9e7079127806274f0b
Anssi Herranen <anssi.herranen@iki.fi>
2018-10-31 16:12:16 +0200

    Updated ShoppingListFactory

--- src/utils/shopping-list.ts
+++ src/utils/shopping-list.ts
@@ -1,14 +1,14 @@
 import * as Joi from 'joi'
 import { v1 as uuidv1 } from 'uuid'
 
-// import Logger from '@utils/logger'
 
 const ITEM_ITEM_ID = 'itemId'
 const ITEM_PURCHASED = 'purchased'
 const ITEM_QUANTITY = 'quantity'
 const ITEM_UNIT = 'unit'
+const LIST_ITEMS = 'items'
 
-interface IShoppingListItemFactoryParams {
+interface IListItemFactoryParams {
   itemId?: string,
   name: string,
   purchased?: boolean,
@@ -16,12 +16,31 @@ interface IShoppingListItemFactoryParams {
   unit?: string,
 }
 
-interface IShoppingListItem {
+interface IListItem {
   itemId: string,
   name: string,
   purchased: boolean,
   quantity: number,
   unit?: string,
+}
+
+interface IShoppingListFactoryParams {
+  listId: string,
+  items?: IListItem[],
+}
+
+interface IShoppingList {
+  listId: string,
+  items: IListItem[],
+}
+
+const validate = (item: object, schema: Joi.Schema) => {
+  const result = Joi.validate(item, schema, { abortEarly: false })
+  if (result.error !== null) {
+    const errorMessage = result.error.details.map((d) => d.message).join('\n')
+    throw new Error(errorMessage)
+  }
+  return item
 }
 
 const shoppingListItemSchema = Joi.object().keys({
@@ -32,19 +51,15 @@ const shoppingListItemSchema = Joi.object().keys({
   [ITEM_UNIT]: Joi.string(),
 })
 
-export const shoppingListItemFactory = (params: IShoppingListItemFactoryParams): IShoppingListItem => {
+export const shoppingListItemFactory = (params: IListItemFactoryParams): IListItem => {
   const itemData = { name: params.name }
   itemData[ITEM_ITEM_ID] = params.hasOwnProperty(ITEM_ITEM_ID) ? params[ITEM_ITEM_ID] : uuidv1()
   itemData[ITEM_PURCHASED] = params.hasOwnProperty(ITEM_PURCHASED) ? params[ITEM_PURCHASED] : false
   itemData[ITEM_QUANTITY] = params.hasOwnProperty(ITEM_QUANTITY) ? params[ITEM_QUANTITY] : 1
   itemData[ITEM_UNIT] = params.hasOwnProperty(ITEM_UNIT) ? params[ITEM_UNIT] : undefined
 
-  const result = Joi.validate(itemData, shoppingListItemSchema, { abortEarly: false })
-  if (result.error !== null) {
-    const errorMessage = result.error.details.map((d) => d.message).join('\n')
-    throw new Error(errorMessage)
-  }
-  return itemData as IShoppingListItem
+  validate(itemData, shoppingListItemSchema)
+  return itemData as IListItem
 }
 
 const shoppingListSchema = Joi.object().keys({
@@ -52,11 +67,9 @@ const shoppingListSchema = Joi.object().keys({
   items: Joi.array().items(shoppingListItemSchema),
 })
 
-export const shoppingListFactory = (params) => {
-  const result = Joi.validate(params, shoppingListSchema, { abortEarly: false })
-  if (result.error !== null) {
-    const errorMessage = result.error.details.map((d) => d.message).join('\n')
-    throw new Error(errorMessage)
-  }
-  return params
+export const shoppingListFactory = (params: IShoppingListFactoryParams): IShoppingList => {
+  const listData = { listId: params.listId }
+  listData[LIST_ITEMS] = params.hasOwnProperty(LIST_ITEMS) ? params[LIST_ITEMS] : []
+  validate(listData, shoppingListSchema)
+  return params as IShoppingList
 }
--- tests/shopping-list.test.ts
+++ tests/shopping-list.test.ts
@@ -1,11 +1,8 @@
-import { /* shoppingListFactory, */ shoppingListItemFactory } from '../src/utils/shopping-list'
+import { shoppingListFactory, shoppingListItemFactory } from '../src/utils/shopping-list'
 
 
-// const APPLE = 'An apple'
-const TWIGS = 'A bunch of twigs'
-// const MINIMAL = 'Minimal items'
 const BUNCHES = 'bunches'
-// const ANSSIS_LIST = `Anssi's list`
+const TWIGS = 'twigs'
 
 const itemWithAllProps = {
   itemId: 'aae7e080-dd08-11e8-b761-e71312b67519',
@@ -19,16 +16,39 @@ const itemWithMinimalProps = {
   name: TWIGS,
 }
 
-test('A valid ShoppingListItem with id is factured.', () => {
+const listWithAllProps = {
+  items: [
+    shoppingListItemFactory(itemWithAllProps),
+    shoppingListItemFactory(itemWithAllProps),
+    shoppingListItemFactory(itemWithAllProps),
+  ],
+  listId: 'my awesome shopping list',
+}
+
+const listWithMinimalProps = {
+  listId: 'my first shopping list',
+}
+
+test('A valid ListItem with id is factured.', () => {
   const item = shoppingListItemFactory(itemWithAllProps)
   expect(item).toEqual(itemWithAllProps)
 })
 
-test('A valid ShoppingListItem with minimal props (only a name) is factured.', () => {
+test('A valid ListItem with minimal props (only a name) is factured.', () => {
   const item = shoppingListItemFactory(itemWithMinimalProps)
   expect(item.itemId).toMatch(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i)
   expect(item.name).toBe(TWIGS)
   expect(item.purchased).toBe(false)
   expect(item.quantity).toBe(1)
   expect(item.unit).toBe(undefined)
+})
+
+test('A valid ShoppingList with items is factured.', () => {
+  const list = shoppingListFactory(listWithAllProps)
+  expect(list).toEqual(listWithAllProps)
+})
+
+test('A valid ShoppingList with minimal props (only a id) is factured.', () => {
+  const list = shoppingListFactory(listWithMinimalProps)
+  expect(list.listId).toEqual(listWithMinimalProps.listId)
 })
